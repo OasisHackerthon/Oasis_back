162eff8b6bd8189f0f029c4abf0653145b7ee5f7
Choi MinJung <mj020503@naver.com>
2023-11-12 00:54:16 +0900

    fix: 인가코드 로직 수정

--- src/main/java/Mirthon/Oasis_back/controller/KakaoController.java
+++ src/main/java/Mirthon/Oasis_back/controller/KakaoController.java
@@ -2,15 +2,13 @@ package Mirthon.Oasis_back.controller;
 
 
 import Mirthon.Oasis_back.config.kakao.KakaoOAuth2;
+import Mirthon.Oasis_back.domain.KakaoAuthRequest;
 import Mirthon.Oasis_back.domain.User;
 import Mirthon.Oasis_back.service.KakaoLoginService;
 import Mirthon.Oasis_back.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true", allowedHeaders = "*")
@@ -31,9 +29,11 @@ public class KakaoController {
 
     // 카카오 서버로부터 받은 인가 코드
     @GetMapping("/auth/kakao/callback")
-    public String kakaoLoginCallback(@RequestParam(value = "code",required = false) String code) {
+    public @ResponseBody String kakaoLoginCallback(@RequestBody KakaoAuthRequest kakaoAuthRequest) {
+        String code = kakaoAuthRequest.getCode();
         userService.kakaoLogin(code);
         User currentUser = userService.getCurrentUser();
-        return "카카오 인증 완료 " + currentUser;
+        // 리액트에게 로그인 성공 여부를 응답할 수 있도록 원하는 데이터를 반환
+        return "카카오 인증 완료 ";
     }
 }
